e1a3c58b071229d467be8c9abd2b313bbdad8af9
joelepore <giuseppejoelepore@gmail.com>
2025-01-18 12:13:40 +0100

    feat: enhance Button component with hover effect

--- src/components/partials/Button.jsx
+++ src/components/partials/Button.jsx
@@ -1,7 +1,7 @@
 const Button = ({ text, className, onClick }) => {
   return (
     <button
-      className={`${className} bg-blue-500 text-white py-1 rounded-2xl`}
+      className={`${className} bg-blue-500 text-white py-1 rounded-2xl hover:bg-blue-700 transition`}
       onClick={onClick}
     >{text}</button>
   )
