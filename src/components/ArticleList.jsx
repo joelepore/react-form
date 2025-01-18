9930ed736563dc90dce15c9e89ebab10ae2f1a5b
joelepore <giuseppejoelepore@gmail.com>
2025-01-18 12:06:27 +0100

    feat: display message when there are no articles

--- src/components/ArticleList.jsx
+++ src/components/ArticleList.jsx
@@ -2,20 +2,27 @@ import Card from "./Card"
 import { IoIosCloseCircle } from "react-icons/io";
 
 const ArticleList = ({ articles, className, onDelete }) => {
+
+  const renderArticles = () => {
+    const mappedArticles = articles.map(article => (
+      <li key={`art-${article.id}`} className="border-b border-neutral-300 article-item">
+        <div className="py-2 px-4 flex items-center justify-between">
+          <div>
+            <h3 className="text-lg font-medium">{article.title}</h3>
+            <p className="text-sm">{`${article.isComplete ? 'Completo' : 'Draft'}`}</p>
+          </div>
+          <IoIosCloseCircle className="text-xl cursor-pointer" onClick={() => onDelete(article.id)} />
+        </div>
+      </li>
+    ));
+
+    return mappedArticles.length == 0 ? <li className="py-2 px-4">Nessun articolo presente</li> : mappedArticles;
+  }
+
   return (
     <Card className={`${className}`}>
       <ul>
-        {articles.map(article => (
-          <li key={`art-${article.id}`} className="border-b border-neutral-300 article-item">
-            <div className="py-2 px-4 flex items-center justify-between">
-              <div>
-                <h3 className="text-lg font-medium">{article.title}</h3>
-                <p className="text-sm">{`${article.isComplete ? 'Completo' : 'Draft'}`}</p>
-              </div>
-              <IoIosCloseCircle className="text-xl cursor-pointer" onClick={() => onDelete(article.id)} />
-            </div>
-          </li>
-        ))}
+        {renderArticles()}
       </ul>
     </Card>
   )
